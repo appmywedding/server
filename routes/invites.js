38c4f07e4ee312e74743a81b8ef323762f7fe59f
OrelsProjects <orelsmail@gmail.com>
2022-03-29 21:08:11 +0300

    - getting uid in the headers work

--- routes/invites.js
+++ routes/invites.js
@@ -3,24 +3,31 @@ var router = express.Router();
 var data = require('../data');
 var db = require('../firebase');
 var paths = require('../constants/paths');
+var { people } = require('../data');
 
 
 router.post('/add', async function (req, res) {
     try {
         const invitedToAdd = getPeopleFromReq(req);
-        const auth = getAuthFromReq(req);
-        const path = paths.invited(auth.uid);
-        await db.addAll(path, invitedToAdd);
+        const uid = getUIDFromReq(req);
+        console.log(uid);
+        if (!uid || (Object.keys(uid).length === 0 && Object.getPrototypeOf(uid) === Object.prototype)) {
+            res.status(400).send({ ex: 'uid is null' });
+            return;
+        }
+        const path = paths.invited(uid);
+        await db.addAll(path, people);
         res.status(200);
         res.send(data.people);
     } catch (ex) {
+        console.log(ex);
         res.status(500).send(ex);
     }
 });
 
 router.post('/remove', async function (req, res) {
     const invitedToRemove = getPeopleFromReq(req);
-    const auth = getAuthFromReq(req);
+    const auth = getUIDFromReq(req);
     const path = paths.invited(auth.uid);
     res.status(200);
     res.send(invitedToRemove);
@@ -34,11 +41,9 @@ router.post('/update', async function (req, res) {
 
 router.get('/getAll', async function (req, res) {
     try {
-        const auth = getAuthFromReq(req);
-        console.log(req);
-        const path = paths.invited(auth.uid);
+        const uid = getUIDFromReq(req);
+        const path = paths.invited(uid);
         const invited = await db.getAll(path);
-        console.log(invited);
         res.status(200).send(invited);
     } catch (ex) {
         res.status(500).send(ex);
@@ -48,6 +53,6 @@ router.get('/getAll', async function (req, res) {
 
 const getPeopleFromReq = (req) => req?.body?.people ? req?.body?.people : {};
 
-const getAuthFromReq = (req) => req?.body?.auth ? req?.body?.auth : {};
+const getUIDFromReq = (req) => req?.headers?.uid ? req?.headers?.uid : {};
 
 module.exports = router;
